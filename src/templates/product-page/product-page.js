fd98f77f131b1fbc1529d2d77be6520047c351da
apple <apple@appurunoiMac.local>
2020-03-02 16:38:07 +0900

    src/templates:コンポーネントごとにディレクトリ変更とproduct-pageのスタイルとclass名変更

--- src/templates/product-page/product-page.js
+++ src/templates/product-page/product-page.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
-import Layout from "../components/Layout";
-import Features from "../components/Features";
-import Testimonials from "../components/Testimonials";
-import Pricing from "../components/Pricing";
-import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
+import Layout from "../../components/Layout";
+import Features from "../../components/Features";
+import Testimonials from "../../components/Testimonials";
+import Pricing from "../../components/Pricing";
+import PreviewCompatibleImage from "../../components/PreviewCompatibleImage";
 import "./product-page.scss";
 
 export const ProductPageTemplate = ({
