1f386f8fea64ee4d1ba58c743a38e8c194817dd7
apple <apple@appurunoiMac.local>
2020-03-02 15:51:23 +0900

    コンフリクトの修正の為にproduct-pageのclass名を修正前に変更

--- src/templates/product-page/product-page.js
+++ src/templates/product-page/product-page.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import Layout from '../../components/Layout'
-import Features from '../../components/Features'
-import Testimonials from '../../components/Testimonials'
-import Pricing from '../../components/Pricing'
-import PreviewCompatibleImage from '../../components/PreviewCompatibleImage'
-import './product-page.scss'
+import Layout from '../components/Layout'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
+import Pricing from '../components/Pricing'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const ProductPageTemplate = ({
   image,
@@ -17,103 +16,85 @@ export const ProductPageTemplate = ({
   main,
   testimonials,
   fullImage,
-  pricing
+  pricing,
 }) => (
-  <div className="product-page__content">
-    <div className='product-page__topimage--container'>
+  <div className="content">
     <div
-      className="product-page__topimage"
+      className="full-width-image-container margin-top-0"
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`
+        })`,
       }}
     >
-      <h2 className="product-page__image--title">
+      <h2
+        className="has-text-weight-bold is-size-1"
+        style={{
+          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+          backgroundColor: '#f40',
+          color: 'white',
+          padding: '1rem',
+        }}
+      >
         {title}
       </h2>
-      </div>
     </div>
-
-
-    <section className="product-page__section">
-      {" "}
-      <div className="product-page__container">
-        <div className="product-page__section">
-          <div className="product-page__columns">
-            <div className="product-page__column ">
-              {" "}
-              <h3 className="product-page__heading">{heading}</h3>{" "}
-              <p className="product-page__description">{description}</p>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-7 is-offset-1">
+              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+              <p>{description}</p>
             </div>
           </div>
-          <div className="product-page__columns">
-            <div className="product-page__column ">
-              {" "}
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
               <Features gridItems={intro.blurbs} />
-              <div className="product-page__columns">
-                <div className="product-page__column ">
-                  {" "}
-                  <h3 className="product-page__main-heading">
-                    {" "}
+              <div className="columns">
+                <div className="column is-7">
+                  <h3 className="has-text-weight-semibold is-size-3">
                     {main.heading}
                   </h3>
-                  <p className="product-page__main-description">
-                    {main.description}
-                  </p>
+                  <p>{main.description}</p>
                 </div>
               </div>
-
-              <div className="product-page__tile--is-ancestor">
-                <div className="product-page__tile--is-vertical">
-                 
-                 
-                  <div className="product-page__tile">
-
-                    <div className="product-page__tile--is-parent1">
-                      <article className="product-page__tile--is-child1">
+              <div className="tile is-ancestor">
+                <div className="tile is-vertical">
+                  <div className="tile">
+                    <div className="tile is-parent is-vertical">
+                      <article className="tile is-child">
                         <PreviewCompatibleImage imageInfo={main.image1} />
                       </article>
                     </div>
-
-                    <div className="product-page__tile--is-parent2">
-                      <article className="product-page__tile--is-child2">
+                    <div className="tile is-parent">
+                      <article className="tile is-child">
                         <PreviewCompatibleImage imageInfo={main.image2} />
                       </article>
                     </div>
                   </div>
-
-
-                  <div className="product-page__tile--is-parent3">
-                    <article className="product-page__tile--is-child3">
+                  <div className="tile is-parent">
+                    <article className="tile is-child">
                       <PreviewCompatibleImage imageInfo={main.image3} />
                     </article>
                   </div>
-
                 </div>
               </div>
-
-
               <Testimonials testimonials={testimonials} />
-              <div className='product-page__image-container'>
               <div
-                className="product-page__full-width-image-container"
+                className="full-width-image-container"
                 style={{
                   backgroundImage: `url(${
                     fullImage.childImageSharp
                       ? fullImage.childImageSharp.fluid.src
                       : fullImage
-                  })`
+                  })`,
                 }}
               />
-              </div>
-              <h2 className="product-page__pricing-heading">
-                {" "}
+              <h2 className="has-text-weight-semibold is-size-2">
                 {pricing.heading}
               </h2>
-              <p className="product-page__pricing-description">
-                {pricing.description}
-              </p>{" "}
+              <p className="is-size-5">{pricing.description}</p>
               <Pricing data={pricing.plans} />
             </div>
           </div>
@@ -121,7 +102,7 @@ export const ProductPageTemplate = ({
       </div>
     </section>
   </div>
-);
+)
 
 ProductPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
