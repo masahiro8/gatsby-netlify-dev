e4ad6e6513b551dca96b7d26db27debc02e1d175
Asane <asane@AsanenoMacBook-Air.local>
2020-03-04 19:10:00 +0900

    blogの制作日の表示を日本語に。blogのトップページでindex-page.jsに直書きしてあったコードの削除

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Layout from '../../components/Layout/Layout'
-import BlogRoll from '../../components/BlogRoll/BlogRoll'
-import './blog-page.scss'
+import Layout from "../../components/Layout/Layout";
+import BlogRoll from "../../components/BlogRoll/BlogRoll";
+import "./blog-page.scss";
 //blogトップページ
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="blog-page__image"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
-        <h1 className="blog-page__title">
-            Latest Stories
-          </h1>
+        <div className="blog-page__image">
+          <h1 className="blog-page__title">Latest Stories</h1>
         </div>
         <section className="blog-page__section">
           <div className="blog-page__container">
@@ -26,6 +19,6 @@ export default class BlogIndexPage extends React.Component {
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
