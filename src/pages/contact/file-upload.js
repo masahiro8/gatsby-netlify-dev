a6ad5e138d0b68b718df3b116ae0613c52e584bc
apple <apple@appurunoiMac.local>
2020-03-04 11:23:32 +0900

    src/componentsのディレクトリ変更とそれに伴うパスの書き換え

--- src/pages/contact/file-upload.js
+++ src/pages/contact/file-upload.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { navigate } from 'gatsby-link'
-import Layout from '../../components/Layout'
+import Layout from '../../components/Layout/Layout'
 import './fileUpload.scss'
 
 function encode(data) {
--- src/templates/about-page/about-page.js
+++ src/templates/about-page/about-page.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import Layout from '../../components/Layout'
-import Content, { HTMLContent } from '../../components/Content'
+import Layout from '../../components/Layout/Layout'
+import Content, { HTMLContent } from '../../components/Content/Content'
 import './about-page.scss'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
