3f0331d0c09a80a4981c17a54ed655bdaeda16a6
apple <apple@appurunoiMac.local>
2020-03-23 18:04:39 +0900

    不要コード削除

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -6,8 +6,6 @@ import './tags.scss'
 // import PropTypes from "prop-types"
 
 //tagspagetop
-
-
 const TagsPage =({  data: {
   allMarkdownRemark: { group },
   site: {
@@ -55,59 +53,3 @@ export default () => (
   />
 )
 
-
-
-
-
-// const TagsPage = ({
-//   data: {
-//     allMarkdownRemark: { group },
-//     site: {
-//       siteMetadata: { title },
-//     },
-//   },
-// }) => (
-//   <Layout>
-//     <section className="section">
-//       <Helmet title={`Tags | ${title}`} />
-//       <h1>tagsページトップ</h1>
-//       <div className="container content">
-//         <div className="columns">
-//           <div
-//             className="column is-10 is-offset-1"
-//             style={{ marginBottom: '6rem' }}
-//           >
-//             <h1 className="title is-size-2 is-bold-light">Tags</h1>
-//             <ul className="taglist">
-//               {group.map(tag => (
-//                 <li key={tag.fieldValue}>
-//                   <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-//                     {tag.fieldValue} ({tag.totalCount})
-//                   </Link>
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   </Layout>
-// )
-
-// export default TagsPage
-
-// export const tagPageQuery = graphql`
-//   query TagsQuery {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     allMarkdownRemark(limit: 1000) {
-//       group(field: frontmatter___tags) {
-//         fieldValue
-//         totalCount
-//       }
-//     }
-//   }
-// `
